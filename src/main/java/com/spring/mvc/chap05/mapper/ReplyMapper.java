ea504618c71848a68ebde6e2956276e9a0781d9c
SJHlll <ssaadd1212@gmail.com>
2024-04-02 12:25:36 +0900

    댓글 목록 페이징 진행

--- src/main/java/com/spring/mvc/chap05/mapper/ReplyMapper.java
+++ src/main/java/com/spring/mvc/chap05/mapper/ReplyMapper.java
@@ -1,7 +1,9 @@
 package com.spring.mvc.chap05.mapper;
 
+import com.spring.mvc.chap05.common.Page;
 import com.spring.mvc.chap05.entity.Reply;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -21,7 +23,12 @@ public interface ReplyMapper {
     Reply findOne(int replyNo);
 
     // 댓글 전체 목록 조회
-    List<Reply> findAll(int boardNo);
+    /* 마이바티스에서 mapper에 매개변수가 2개 이상일 때는 변수명으로 바로 지목할 수 없다
+    * 1. 변수를 하나만 쓴다 (전달하고자 하는 값이 여러 개라면 객체로 한번에 포장해서 넘긴다
+    * 2. Map으로 포장해서 넘긴다 (Map의 key가 xml에서 지목하는 이름이 된다)
+    * 3. @Param을 사용해서 직접 이름 붙이기 */
+
+    List<Reply> findAll(@Param("bn") int boardNo, @Param("p") Page page);
 
     // 댓글 총 개수 조회
     int count(int boardNo);
