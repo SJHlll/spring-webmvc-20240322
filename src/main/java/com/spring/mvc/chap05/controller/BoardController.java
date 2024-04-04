7b03ab7dc4f454211ea7ae4aa8b872a53a181046
SJHlll <ssaadd1212@gmail.com>
2024-04-04 15:41:18 +0900

    게시판 글쓰기, 삭제 인가처리

--- src/main/java/com/spring/mvc/chap05/controller/BoardController.java
+++ src/main/java/com/spring/mvc/chap05/controller/BoardController.java
@@ -7,7 +7,9 @@ import com.spring.mvc.chap05.dto.request.BoardWriteRequestDTO;
 import com.spring.mvc.chap05.dto.response.BoardDetailResponseDTO;
 import com.spring.mvc.chap05.dto.response.BoardListResponseDTO;
 import com.spring.mvc.chap05.service.BoardService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,7 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/board")
+@Slf4j
 public class BoardController {
 
     private final BoardService service;
@@ -28,17 +31,17 @@ public class BoardController {
         System.out.println("search = " + page);
         List<BoardListResponseDTO> dtoList = service.getList(page);
 
-        /* 페이징 버튼 알고리즘 적용
-        * 사용자가 요청한 페이지 정보, 총 게시물 개수를 전달
-        * 페이지 알고리즘 자동 호출 */
+        // 페이징 버튼 알고리즘 적용 -> 사용자가 요청한 페이지 정보, 총 게시물 개수를 전달.
+        // 페이징 알고리즘 자동 호출.
         PageMaker pageMaker = new PageMaker(page, service.getCount(page));
 
-        /* model에 글 목록 분만 아니라 페이지 버튼 정보도 같이 담아서 전달한다 */
+        // model에 글 목록 뿐만 아니라 페이지 버튼 정보도 같이 담아서 전달하자.
         model.addAttribute("bList", dtoList);
         model.addAttribute("maker", pageMaker);
 
-        /* 메서드의 파라미터 값을 model 객체에 바로 추가하고 싶다면 @ModelAttribute를 사용*/
-//        model.addAttribute("s", page)
+        // 메서드의 파라미터값을 model객체에 바로 추가하고 싶다면 @ModelAttribute를 사용.
+//        model.addAttribute("s", page);
+
         return "chap05/list";
     }
 
@@ -54,11 +57,10 @@ public class BoardController {
     // BoardWriteRequestDTO를 활용하여 파라미터 처리 -> dto.request 패키지에 생성
     // 등록 완료 후에는 목록 조회 요청이 다시 들어오게끔 처리.
     @PostMapping("/write")
-    public String write(BoardWriteRequestDTO dto) {
-        System.out.println("/board/write: POST!");
-        System.out.println("dto = " + dto);
+    public String write(BoardWriteRequestDTO dto, HttpSession session) {
+        log.info("/board/write: POST, dto: {}", dto);
 
-        service.register(dto);
+        service.register(dto, session);
         return "redirect:/board/list";
     }
 
@@ -67,7 +69,7 @@ public class BoardController {
     // 글번호 전달되면 삭제 진행
     @GetMapping("/delete")
     public String delete(int bno) {
-        System.out.println("/board/delete: GET! " + bno);
+        System.out.println("/board/delete: GET!! " + bno);
         service.delete(bno);
 
         return "redirect:/board/list";
--- src/main/java/com/spring/mvc/chap05/service/MemberService.java
+++ src/main/java/com/spring/mvc/chap05/service/MemberService.java
@@ -5,6 +5,7 @@ import com.spring.mvc.chap05.dto.request.SignUpRequestDTO;
 import com.spring.mvc.chap05.dto.response.LoginUserResponseDTO;
 import com.spring.mvc.chap05.entity.Member;
 import com.spring.mvc.chap05.mapper.MemberMapper;
+import com.spring.mvc.util.LoginUtils;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,7 +24,7 @@ public class MemberService {
     public void join(SignUpRequestDTO dto) {
 
         // 클라이언트가 보낸 회원가입 데이터를
-        // 패스워드 인코딩하여 엔터티로 변환해서 전달
+        // 패스워드 인코딩하여 엔터티로 변환해서 전달.
 //        String encodedPw = encoder.encode(dto.getPassword());
 //        dto.setPassword(encodedPw);
         memberMapper.save(dto.toEntity(encoder));
@@ -32,42 +33,41 @@ public class MemberService {
 
     // 로그인 검증 처리
     public LoginResult authenticate(LoginRequestDTO dto) {
+
         Member foundMember = memberMapper.findMember(dto.getAccount());
 
-        if (foundMember == null) { // 회원가입 안한 상태
-            System.out.println(dto.getAccount() + "는(은) 없는 아이디 입니다.");
+        if (foundMember == null ) { // 회원가입 안한 상태
+            System.out.println(dto.getAccount() + "(은)는 없는 아이디!");
             return NO_ACC;
         }
 
         // 비밀번호 일치 검사
-        String inputPassword = dto.getPassword(); // 사용자가 입력한 패스워드
+        String inputPassword = dto.getPassword(); // 사용자 입력 패스워드
         String realPassword = foundMember.getPassword(); // 실제 패스워드
 
-        // matches (입력한 비밀번호, 암호화된 비밀번호 -> 일치하면 true, 아니면 false)
-        // equals로 하면 안됨
+        // matches(입력비번, 암호화된 비번) -> 둘이 일치하면 true, 일치하지 않으면 false
+        // equals로 비교하시면 안돼요!
         if (!encoder.matches(inputPassword, realPassword)) {
-            System.out.println("비밀번호가 다릅니다.");
+            System.out.println("비밀번호가 다르다!");
             return NO_PW;
         }
 
-        System.out.println(dto.getAccount() + " 로그인 성공");
+        System.out.println(dto.getAccount() + "님 로그인 성공!");
         return SUCCESS;
-
     }
 
+
     public boolean checkDuplicateValue(String type, String keyword) {
-
         return memberMapper.isDuplicate(type, keyword);
-
     }
 
     public void maintainLoginState(HttpSession session, String account) {
 
         // 세션은 서버에서만 유일하게 보관되는 데이터로서
-        // 로그인 유지 등상태 유지가 필요할 때 사용되는 내장 객체이다.
-        // 세션은 쿠키와 달리 모든 데이터를 저장할 수 있으며 크기도 제한이 없다
-        // 세션의 수명은 기본 1800초 -> 원하는 만큼 수명을 설정할 수 있다
-        // 브라우저가 종료되면 남은 수명에 상관없이 세션 데이터는 소멸된다
+        // 로그인 유지 등 상태 유지가 필요할 때 사용되는 내장 객체입니다.
+        // 세션은 쿠키와 달리 모든 데이터를 저장할 수 있으며 크기도 제한이 없습니다.
+        // 세션의 수명은 기본 1800초 -> 원하는 만큼 수명을 설정할 수 있습니다.
+        // 브라우저가 종료되면 남은 수명에 상관없이 세션 데이터는 소멸합니다.
 
         // 현재 로그인한 회원의 모든 정보 조회
         Member foundMember = memberMapper.findMember(account);
@@ -77,27 +77,13 @@ public class MemberService {
                 .account(foundMember.getAccount())
                 .name(foundMember.getName())
                 .email(foundMember.getEmail())
+                .auth(foundMember.getAuth().getDescription())
                 .build();
 
         // 세션에 로그인한 회원 정보를 저장
-        session.setAttribute("login", dto);
+        session.setAttribute(LoginUtils.LOGIN_KEY, dto);
         // 세션 수명 설정
-        session.setMaxInactiveInterval(60 * 60); // 한시간
+        session.setMaxInactiveInterval(60 * 60); // 1시간
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
