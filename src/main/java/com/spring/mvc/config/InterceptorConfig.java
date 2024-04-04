a6ed1beb52a5b80fc94cb133bada686547dbb051
SJHlll <ssaadd1212@gmail.com>
2024-04-04 17:31:26 +0900

    자동로그인 인터셉터 설정

--- src/main/java/com/spring/mvc/config/InterceptorConfig.java
+++ src/main/java/com/spring/mvc/config/InterceptorConfig.java
@@ -1,6 +1,7 @@
 package com.spring.mvc.config;
 
 import com.spring.mvc.interceptor.AfterLoginInterceptor;
+import com.spring.mvc.interceptor.AutoLoginInterceptor;
 import com.spring.mvc.interceptor.BoardInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
 
     private final AfterLoginInterceptor afterLoginInterceptor;
     private final BoardInterceptor boardInterceptor;
+    private final AutoLoginInterceptor autoLoginInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -24,5 +26,10 @@ public class InterceptorConfig implements WebMvcConfigurer {
                 .addInterceptor(boardInterceptor)
                 .addPathPatterns("/board/*")
                 .excludePathPatterns("/board/list", "board/detail"); // 제외
+
+        // 자동 로그인 인터셉터 설정
+        registry
+                .addInterceptor(autoLoginInterceptor)
+                .addPathPatterns("/**");
     }
 }
